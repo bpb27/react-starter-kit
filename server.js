c6fb8b2e5330f2576381b05f790e9fb513c6c6ed
Brendan Brown <brendan@arcadiapower.com>
2020-09-23 15:04:34 -0700

    more babel features, server upgrades

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
+require('dotenv').config();
+const { NODE_ENV, PORT } = process.env;
+
 const express = require('express');
 const favicon = require('express-favicon');
-const path = require('path');
 const staticGzip = require('express-static-gzip');
+const cors = require('cors');
+const path = require('path');
+const helmet = require('helmet');
 
 const app = express();
+const port = PORT || 3000;
+const isProd = NODE_ENV === 'prod';
 const pathDist = path.join(__dirname, './dist');
 const pathPublic = path.join(__dirname, './public');
-const port = process.env.PORT || 3000;
 
-// serve gzipped static assets in dist and public folders
+// security headers
+// app.use(helmet.contentSecurityPolicy()); // need to whitelist a number of scripts and styles
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.hsts());
+app.use(helmet.ieNoOpen());
+app.use(helmet.noSniff());
+app.use(helmet.permittedCrossDomainPolicies());
+app.use(helmet.referrerPolicy());
+app.use(helmet.xssFilter());
+
+// serve static assets in dist and public folders
 app.use(favicon(`${pathPublic}/favicon.ico`));
 app.use(staticGzip(pathDist));
 app.use(staticGzip(pathPublic));
 
-// health route
+// disable CORS restriction in dev mode
+if (!isProd) app.use(cors());
+
+// health check route
 app.get('/ping', (req, res) => {
   res.send('pong');
 });
@@ -23,4 +45,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(`${pathDist}/index.html`));
 });
 
-app.listen(port, () => console.log(`running on port ${port}`)); // eslint-disable-line no-console
+// start app
+app.listen(port, () => {
+  console.log(`running on port ${port}`);
+});
